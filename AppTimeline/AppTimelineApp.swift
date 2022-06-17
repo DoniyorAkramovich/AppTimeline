296ba440dab6b54e3381426ed9b340270026c8a7
DoniyorAkramovich <berd.doniyor@gmail.com>
2022-06-17 23:39:34 +0500

    Initial Commit

---
+++ AppTimeline/AppTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTimelineApp.swift
+//  AppTimeline
+//
+//  Created by Doniyor on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AppTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
